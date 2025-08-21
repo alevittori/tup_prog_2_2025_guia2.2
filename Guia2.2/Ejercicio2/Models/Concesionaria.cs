624b779208a92ed18c75dc88dcba61a790318942
alevitt <alejandrovitt@hotmail.com>
2025-08-21 12:15:48 -0300

    Comenzamos con el ejercico 2 concecionaria, embarques

---
+++ Guia2.2/Ejercicio2/Models/Concesionaria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    internal class Concesionaria
+    {
+        public double PorcentajeDepreciacion { get; }
+        public int AñoActual {  get; }
+        public double ImporteEnEmbarque { get; }
+        public  double CantidadEmbarques { get; }
+        public Embarque MayorCantidadMotos { get; }
+
+
+        Embarque[] embarques;
+
+        public Concesionaria(int añoActual) { }
+
+        public void IngresarEmbarque(Embarque nuevo) { }
+        public Embarque VerEmbarque(int idx) {  return null; }
+        public Embarque VerEmbarquePorNumero( int numero ) { return null; }
+        public Embarque[] ListaOrdenadaEmbarques() { return null; }
+
+
+    }
+}
---
+++ Guia2.2/Ejercicio2/Models/Embarque.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    internal class Embarque
+    {
+        double porcDepreciacionUsado;
+        int añoEvaluacion;
+
+        public int Numero { get; } //readonly
+        public int CantidadMotos { get; }
+        public int MontoTotal { get; }
+        public double PromedioCosto { get; }
+        public double AntiguedadPromedio {  get; }
+
+        public Embarque(int numero, double porcDepreciacion, int añoEvaluacion) { }
+
+        public double RegistrarMoto(int añosFabricacion, double montoFabricacion) {  return 0; }
+
+    }
+}
